d083a27ec6b140cda7269a7f8931dc4a0e2fbd5b
erikarvidsson <erik.arvidsson.89@gmail.com>
2019-09-17 11:02:15 +0200

    added a side scroll and vide title

---
+++ src/components/SideScroll/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import styled from "styled-components";
+
+const Div = styled.div`
+    display: flex;
+    width: 100vw;
+    overflow-x: auto;
+`;
+
+const SideScroll = (props) => {
+  return (
+    <Div>
+      {props.children}
+    </Div>
+)}
+
+export default SideScroll;
--- src/components/YPlayer/index.js
+++ src/components/YPlayer/index.js
@@ -3,9 +3,10 @@ import ReactPlayer from 'react-player'
 import styled from "styled-components";
 
 const PlayerWrapper = styled.div`
-    position: relative;
-    height: 100%;
-    /* padding-top: 56.25%; 720 / 1280 = 0.5625 */
+  position: relative;
+  height: 100%;
+  top: 18vh;
+  /* padding-top: 56.25%; 720 / 1280 = 0.5625 */
 `;
 
 const YPlayer = (props) => {
@@ -15,8 +16,7 @@ const YPlayer = (props) => {
       url={props.link}
       className='react-player'
       playing
-      width='80vw'
-      height='80vh'
+      width='100vw'
     />
   </PlayerWrapper>)
   }

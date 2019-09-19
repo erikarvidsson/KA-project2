cc0c67479477319ebfb227c0a90efbcf292b3b56
hannaell <hannaell91@gmail.com>
2019-09-19 16:38:48 +0200

    Fix link styling

--- src/view/library.js
+++ src/view/library.js
@@ -4,12 +4,21 @@ import { Route, Switch, Link } from "react-router-dom";
 import Layout from '../components/Layout';
 import ProfileIcon from '../components/ProfileIcon';
 import NavigationButton from '../components/NavigationButton';
+import { Header, P } from '../components/typo';
 
 
 const BackIcon = styled.img `
     height: 20px;
     width: 20px;
 `
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: white;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`;
 
 const Library = (props) => {
     return(
@@ -17,10 +26,11 @@ const Library = (props) => {
             <Layout>
                 <BackIcon src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)}/>
                 <ProfileIcon />
-                <h1>Mitt bibliotek</h1>
-                <Link to='/Categories'><NavigationButton goTo='Kategorier' img='assets/icons/right-arrow.svg'/></Link>
-                <Link to='/Episodes'><NavigationButton goTo='Avsnitt' img='assets/icons/right-arrow.svg'/></Link>
-                <Link to='/Favorites'><NavigationButton goTo='Favoriter' img='assets/icons/right-arrow.svg'/></Link>
+                {/* top sätter margin-top värdet */}
+                <Header text='Mitt bibliotek' top='12px'/>
+                <StyledLink to='/Categories'><NavigationButton goTo='Kategorier' img='assets/icons/right-arrow.svg'/></StyledLink>
+                <StyledLink to='/Episodes'><NavigationButton goTo='Avsnitt' img='assets/icons/right-arrow.svg'/></StyledLink>
+                <StyledLink to='/Favorites'><NavigationButton goTo='Favoriter' img='assets/icons/right-arrow.svg'/></StyledLink>
             </Layout>
         </div>
     )

64b1aefb804d2a87978d5c0fa5ec0dc4ce67940e
RebeccaKlaening <klaeningrebecca@gmail.com>
2019-09-23 15:49:46 +0200

    added some more css to settingspage

--- src/components/NavigationButton/index.js
+++ src/components/NavigationButton/index.js
@@ -2,44 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 const DivStyled = styled.div`
-    height: 51px;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
+height: 51px;
+width: 100vw;
+display: flex;
+justify-content: center;
 `;
 
 const UlStyled = styled.ul`
-    height: 100%;
-    width: 86vw;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    align-content: center;
-    text-decoration: none;
-    margin: 0;
-    padding: 0;
+height: 100%;
+width: 86vw;
+display: flex;
+justify-content: space-between;
+align-items: center;
+align-content: center;
+text-decoration: none;
+margin: 0;
+padding: 0;
 `;
 
 const LiStyled = styled.li`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    list-style: none;
-
+width: 100%;
+display: flex;
+justify-content: space-between;
+list-style: none;
 `;
 
 const ImgStyled = styled.img `
-    height: 25px;
+height: 25px;
 `;
-
 const NavigationButton = (props) => {
     return(
         <DivStyled>
           <UlStyled>
             <LiStyled>{props.goTo}<ImgStyled src={props.img} alt=''></ImgStyled></LiStyled>
           </UlStyled>
-        </DivStyled>  
-
+        </DivStyled>
     )
 }
 export default NavigationButton;

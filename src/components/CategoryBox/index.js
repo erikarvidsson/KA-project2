4e594a6eaca7855ef447c0994d5cbd1cc9527c0f
hannaell <hannaell91@gmail.com>
2019-09-24 10:43:21 +0200

    Clean up in code

--- src/components/CategoryBox/index.js
+++ src/components/CategoryBox/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { H3, P } from "../typo";
+import React from "react";
+import { P } from "../typo";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -12,14 +12,12 @@ const Container = styled.div`
   top: 0;
   left: 0;
   display: flex;
-  
   align-items: center;
 `;
 
 const Img = styled.img`
   position: absolute;
   object-fit: cover;
-  /* font-size: 22px; */
   width: 100%;
   left: 0;
   top: auto;
@@ -27,12 +25,11 @@ const Img = styled.img`
 `;
 
 export const CategoryBox = props => {
-
         
-         return (
-           <Container {...props} onClick={props.name}>
-             <P top="0px" textAlign='center' marginLeft='0' text={props.title}></P>
-             <Img src={props.src} />
-           </Container>
-         );
-       };
+    return (
+        <Container {...props} onClick={props.name}>
+            <P top="0px" textAlign='center' marginLeft='0' text={props.title}></P>
+            <Img src={props.src} />
+        </Container>
+    );
+};

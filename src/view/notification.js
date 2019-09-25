a18ca2a4b438773d3896ea5a1000f154dfcc70ad
hannaell <hannaell91@gmail.com>
2019-09-25 19:52:52 +0200

    Clean up code
    App.css deleted padding top
    notifications.js add margin top and fix some css

--- src/view/notification.js
+++ src/view/notification.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import Layout from '../components/Layout';
 import { P } from '../components/typo';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import MenuTop from '../components/MenuTop';
 import Line from '../components/Line';
 
 const DivStyled = styled.div`
-    margin-top: 135px;
+    margin-top: 194px;
     width: 100vw;
 `;
 
@@ -22,12 +22,11 @@ const DivOutputStyled = styled.div`
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
-
 `;
 
 const DivPStyled = styled.div`
     height: 100%;
-    width: 173px;
+    width: 164px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -35,88 +34,89 @@ const DivPStyled = styled.div`
 `;
 
 const Img = styled.img`
-  object-fit: cover;
-  width: 28vw;
-  height: 28vw;
-  border-radius: 50%;
+    object-fit: cover;
+    width: 28vw;
+    height: 28vw;
+    border-radius: 50%;
 `;
 
 const Notification = () => {
 
+
     const [friends, setFriends] = useState([
-      {
-        name: "Camille Jadermark",
-        img: "assets/imgs/Camille.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Freja Candell",
-        img: "assets/imgs/Freja.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Otto Bäckström",
-        img: "assets/imgs/Otto.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Hanna Ell",
-        img: "assets/imgs/Hanna.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Mauro Cilli",
-        img: "assets/imgs/Mauro.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Rebecca Klaening",
-        img: "assets/imgs/Rebecca.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Karin Holmquist",
-        img: "assets/imgs/Erik.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      },
-      {
-        name: "Erik Arvidsson",
-        img: "assets/imgs/Erik.jpg",
-        text: "lade till",
-        title: 'Grunden i koordinerade kropprörelser.',
-        text2: 'i sina favoriter.'
-      }
+        {
+            name: 'Camille Jadermark',
+            img: 'assets/imgs/Camille.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Freja Candell',
+            img: 'assets/imgs/Freja.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Otto Bäckström',
+            img: 'assets/imgs/Otto.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Hanna Ell',
+            img: 'assets/imgs/Hanna.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Mauro Cilli',
+            img: 'assets/imgs/Mauro.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Rebecca Klaening',
+            img: 'assets/imgs/Rebecca.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Karin Holmquist',
+            img: 'assets/imgs/Erik.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        },
+        {
+            name: 'Erik Arvidsson',
+            img: 'assets/imgs/Erik.jpg',
+            text: 'lade till',
+            title: 'Grunden i koordinerade kropprörelser.',
+            text2: 'i sina favoriter.'
+        }
     ]);
 
     return(
         <div>
             <Layout>
-                <MenuTop text='Händelser'/>
+                <MenuTop text='Händelser' />
                 <DivStyled>
                     {friends.map(friend => {
                         return (
                             <DivOutput>
                                 <DivOutputStyled>
-                                    <Img src={friend.img} alt="" />
+                                    <Img src={friend.img} alt='' />
                                     <DivPStyled>
-                                        <P text={friend.name} fontWeight='bold' marginTop='0' marginBottom='0'></P>
-                                        <P text={friend.text} marginTop='0' marginBottom='0'></P>
-                                        <P text={friend.title} fontWeight='bold' marginTop='0' marginBottom='0'/>
-                                        <P text={friend.text2} marginTop='0' marginBottom='0'/>
+                                        <P text={friend.name} fontWeight='bold' marginTop='0' marginBottom='0' />
+                                        <P text={friend.text} marginTop='0' marginBottom='0' />
+                                        <P text={friend.title} fontWeight='bold' marginTop='0' marginBottom='0' />
+                                        <P text={friend.text2} marginTop='0' marginBottom='0' />
                                     </DivPStyled>
                                 </DivOutputStyled>
                                 <Line />
@@ -126,7 +126,7 @@ const Notification = () => {
                 </DivStyled>
             </Layout>
         </div>
-    )
+    );
 }
 
 export default Notification;

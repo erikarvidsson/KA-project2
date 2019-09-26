e159f67f38a66d5885745fe05c487b09f85e69b2
hannaell <hannaell91@gmail.com>
2019-09-26 16:56:43 +0200

    clean up in code
    index.js clean up in code, fix css and change img

--- src/components/ProfileIcon/index.js
+++ src/components/ProfileIcon/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Icon = styled.img`
-  height: 30px;
-  width: 30px;
-  margin-right: 20px;
+  width: 32px;
+  border-radius: 50%;
+  margin-right: 22px;
   margin-top: 10px;
-`
+`;
 
 const ProfileIcon = () => {
-    return(
+    return (
         <div>
-            <Link to='/Profile'><Icon src='assets/icons/Profilbild.svg'/></Link>
+            <Link to='/Profile'><Icon src='assets/imgs/profilbild.png'/></Link>
         </div>
-    )
+    );
 }
 
 export default ProfileIcon;

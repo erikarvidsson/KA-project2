89a12560497f11fa2dde27631a5ab68bb817d3c1
erikarvidsson <erik.arvidsson.89@gmail.com>
2019-09-26 11:10:02 +0200

    fixed latest page

--- src/view/latest.js
+++ src/view/latest.js
@@ -5,12 +5,14 @@ import VideoData from "../data/youtube.json";
 import { MediaBox } from '../components/MediaBox';
 import ProfileIcon from "../components/ProfileIcon";
 import { H3 } from "../components/typo";
+import YPlayer from "../components/YPlayer";
 
 const LayoutStyled = styled.div`
-    height: 400vw;
+    /* height: 400vw; */
 `;
 
-const DivStyled = styled.div `
+const DivStyled = styled.div `  
+    position: relative;
     width: 100vw;
     height: 56px;
     display: flex;
@@ -25,49 +27,63 @@ const BackIconStyled = styled.img`
 `;
 
 const SectionDivStyled = styled.div`
-    height: 300px;
+  margin-top: 36px;
+  position: relative;
+  /* height: 300px; */
 `;
 
 const Latest = (props) => {
+    console.log(VideoData[0].title);
     return (
-        <div>
-            <Layout>
-                <LayoutStyled>
-                    <DivStyled>
-                        <BackIconStyled src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)}/>
-                        <H3 text='Senast tillagda' textAlign='center' marginLeft='18px' marginRight='0px'/>
-                        <ProfileIcon />
-                    </DivStyled>
-                    <SectionDivStyled>
-                        <MediaBox
-                            src={VideoData[0].thumbnail}
-                            title={VideoData[0].title}
-                            description={VideoData[0].description}
-                            position="relative"
-                            ZIndex="-1"
-                        />
-                    </SectionDivStyled>              
-                    <SectionDivStyled>
-                        <MediaBox
-                            src={VideoData[1].thumbnail}
-                            title={VideoData[1].title}
-                            description={VideoData[1].description}
-                            position="relative"
-                            ZIndex="-1"
-                        />
-                    </SectionDivStyled>
-                    <SectionDivStyled>
-                        <MediaBox
-                            src={VideoData[2].thumbnail}
-                            title={VideoData[2].title}
-                            description={VideoData[2].description}
-                            position="relative"
-                            ZIndex="-1"
-                        />
-                    </SectionDivStyled>
-                </LayoutStyled>
-            </Layout>
-        </div>
+      <div>
+        <Layout>
+          <LayoutStyled>
+            <DivStyled>
+              <BackIconStyled
+                src="assets/icons/left-arrow.svg"
+                onClick={() => props.history.goBack(props)}
+              />
+              <H3
+                position="relative"
+                text="Senast tillagda"
+                textAlign="center"
+                marginLeft="18px"
+                marginRight="0px"
+                ZIndex="2"
+              />
+              <ProfileIcon />
+            </DivStyled>
+            <SectionDivStyled>
+              <YPlayer
+                link={VideoData[0].url}
+                hText={VideoData[0].title}
+                pText={VideoData[0].description}
+                position="relative"
+                marginTop="28px"
+                ZIndex="10"
+              />
+            </SectionDivStyled>
+            <SectionDivStyled>
+              <YPlayer
+                link={VideoData[1].url}
+                hText={VideoData[1].title}
+                pText={VideoData[1].description}
+                position="relative"
+                ZIndex="2"
+              />
+            </SectionDivStyled>
+            <SectionDivStyled>
+              <YPlayer
+                link={VideoData[2].url}
+                hText={VideoData[2].title}
+                pText={VideoData[2].description}
+                position="relative"
+                ZIndex="2"
+              />
+            </SectionDivStyled>
+          </LayoutStyled>
+        </Layout>
+      </div>
     );
 }
 

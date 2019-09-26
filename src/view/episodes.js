46155748daf1479dde174c1c941b41fce87f8550
hannaell <hannaell91@gmail.com>
2019-09-26 19:20:10 +0200

    clean up code
    episodes.js clean up in code and add margins

--- src/view/episodes.js
+++ src/view/episodes.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import Layout from '../components/Layout';
 import ProfileIcon from '../components/ProfileIcon';
-import { Header, P, H2, H3 } from '../components/typo';
+import { H3 } from '../components/typo';
 import ModalColumn from '../components/ModalColumn';
 import SoundData from '../data/playlists.json';
 import AudioPlayer from '../components/AudioPlayer';
@@ -28,31 +28,20 @@ const Episodes = (props) => {
   const [show, setShow] = useState(false)
 
     return(
-          <Layout>
+        <Layout>
             <DivStyled>
-                <BackIconStyled src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)}/>
-                <H3 text='Avsnitt' textAlign='center' marginRight='0px'/>
-              <ProfileIcon />
+                <BackIconStyled src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)} />
+                <H3 text='Avsnitt' textAlign='center' marginRight='0px' marginLeft='6px' />
+                <ProfileIcon />
             </DivStyled>
-
-              {SoundData.map(sound => {
+            {SoundData.map(sound => {
                 return (
-                  <ModalColumn
-                    url={sound.thumbnail}
-                    title={sound.title}
-                    description={sound.description}>
-
-                  <AudioPlayer
-                    pageName={Episodes}
-                    text={sound.title}
-                    thumbnail={sound.img}
-                    src='/audio/audiofile1.mp3'>
-                  </AudioPlayer>
-
-                </ModalColumn>
-              );
+                    <ModalColumn url={sound.thumbnail} title={sound.title} description={sound.description}>
+                        <AudioPlayer pageName={Episodes} text={sound.title} thumbnail={sound.img} src='/audio/audiofile1.mp3' />
+                    </ModalColumn>
+                );
             })}
-          </Layout>
+        </Layout>
     )
 }
 

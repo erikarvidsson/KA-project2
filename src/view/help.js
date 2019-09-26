4dbdff4e6b2aebd8c0392de5522549ca8afa8038
hannaell <hannaell91@gmail.com>
2019-09-26 12:44:17 +0200

    clean up in code
    help.js clean up in code

--- src/view/help.js
+++ src/view/help.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import Layout from '../components/Layout';
-import styled from "styled-components";
 import { P, H3 } from '../components/typo';
 
-const DivStyled = styled.div `
+const DivStyled = styled.div`
     width: 100vw;
     height: 56px;
     display: flex;
     justify-content: space-between;
     align-items: center;
     margin: 0;
+    margin-top: 59px;
 `;
 
 const Div = styled.div`
@@ -18,30 +19,28 @@ const Div = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 31px;
-    margin-left: 20px;
+    margin-left: 17px;
 `;
 
 const BackIconStyled = styled.img`
-    height: 25px;
-    width: 25px;
-    margin-left: 10px;
+    width: 30px;
+    margin-left: 21px;
+    z-index: 800;
 `;
 
 const Help = (props) => {
     return(
-        <div>
-                <Layout>
-                <DivStyled>
-                    <BackIconStyled src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)}/>
-                    <H3 text='Hjälp' textAlign='center' marginLeft="-34px" marginRight='0px'/>
-                </DivStyled>
-                <Div>
-                    <H3 text="Har du bekymmer eller vill fråga något?"/>
-                    <P text="Maila oss så hjälper vi gärna till!"/>
-                    <P text="info@kulturakademin.com" fontWeight="Bold"/>
-                </Div>
-            </Layout>
-        </div>
+        <Layout>
+            <DivStyled>
+                <BackIconStyled src='assets/icons/left-arrow.svg' onClick={() => props.history.goBack(props)} />
+                <H3 text='Hjälp' textAlign='center' marginLeft='-45px' marginRight='0px' />
+            </DivStyled>
+            <Div>
+                <H3 text='Har du bekymmer eller vill fråga något?' />
+                <P text='Maila oss så hjälper vi gärna till!' />
+                <P text='info@kulturakademin.com' fontWeight='bold' marginTop='0px' />
+            </Div>
+        </Layout>
     );
 }
 
